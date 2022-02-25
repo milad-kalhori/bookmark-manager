db6b501a02b1d4bc2484ec4e893a9fdafdc1d570
milad-kalhori <milad7kalhori@gmail.com>
2022-02-25 23:53:00 +0330

    deleted api user

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ const server = app.listen(Port , ()=> {
 
 // http://localhost:5000
 app.use('/api/v2/admin/library', require('./src/routes/admin/library'))
-app.use('/api/v2/user/library', require('./src/routes/user/library'))
 
 process.on('unhandledRejection',(reason,reject)=> {
     let promiseCauseProblem = reason.stack.split('\n ')
