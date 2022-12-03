02381970734d47d5e6a079a97f46620e58861acd
milad-kalhori <milad7kalhori@gmail.com>
2022-12-03 21:07:49 +0330

    added user to transaction

--- src/DB/models/transaction.js
+++ src/DB/models/transaction.js
@@ -3,6 +3,12 @@ const mongoose = require('mongoose')
 const transactionSchema = mongoose.Schema({
   state: String,
   level: String,
+  user: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User'
+    }
+  ],
 }, {timestamps: true})
 
 
