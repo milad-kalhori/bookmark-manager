376e8d7934e236b0245fd34e239266575307b4d6
milad-kalhori <milad7kalhori@gmail.com>
2022-12-03 21:09:53 +0330

    added internalTrackingNumber totransaction

--- src/DB/models/transaction.js
+++ src/DB/models/transaction.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const transactionSchema = mongoose.Schema({
   state: String,
   level: String,
+  internalTrackingNumber: Number,
   user: [
     {
       type: mongoose.Schema.ObjectId,
