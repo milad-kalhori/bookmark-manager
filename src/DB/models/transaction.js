3b077659fe4da0e3d82c2baba4acdbbdf950f445
milad-kalhori <milad7kalhori@gmail.com>
2022-12-03 21:05:24 +0330

    added timestamps to transaction

--- src/DB/models/transaction.js
+++ src/DB/models/transaction.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const transactionSchema = mongoose.Schema({
 
-})
+}, {timestamps: true})
 
 
 module.exports = mongoose.model('Transation', transactionSchema)
