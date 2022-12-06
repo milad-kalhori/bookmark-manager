2d44952d602b7ece981badee8155f83b11fc08c0
milad-kalhori <milad7kalhori@gmail.com>
2022-12-06 23:23:30 +0330

    added provider

--- src/bookmarks/bookmarks.controller.ts
+++ src/bookmarks/bookmarks.controller.ts
@@ -1,4 +1,11 @@
 import { Controller } from '@nestjs/common';
+import { BookmarksService } from './bookmarks.service';
 
 @Controller('bookmarks')
-export class BookmarksController {}
+export class BookmarksController {
+  constructor(private bookmarkService: BookmarksService){}
+
+  test(){
+    return this.bookmarkService
+  }
+}
