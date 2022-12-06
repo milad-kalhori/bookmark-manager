d39e2fc1afd02f8fea05d624b5bc880c6c7c2d0a
milad-kalhori <milad7kalhori@gmail.com>
2022-12-06 23:18:40 +0330

    deleted controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookmarksModule } from './bookmarks/bookmarks.module';
-import { MiladModule } from './milad/milad.module';
 
 @Module({
-  imports: [BookmarksModule, MiladModule],
+  imports: [BookmarksModule],
   controllers: [AppController],
   providers: [AppService],
 })
