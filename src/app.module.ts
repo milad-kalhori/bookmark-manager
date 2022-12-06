3a6f7a806489a5043e41e60304c2997d1ac60bb1
milad-kalhori <milad7kalhori@gmail.com>
2022-12-06 22:39:24 +0330

    added bookmarks module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BookmarksModule } from './bookmarks/bookmarks.module';
+import { MiladModule } from './milad/milad.module';
 
 @Module({
-  imports: [],
+  imports: [BookmarksModule, MiladModule],
   controllers: [AppController],
   providers: [AppService],
 })
