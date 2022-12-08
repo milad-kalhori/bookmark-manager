4df2854cc016c263b8c86311ff905352051e36b8
milad-kalhori <milad7kalhori@gmail.com>
2022-12-08 10:46:05 +0330

    addded findAlll

--- src/bookmarks/bookmarks.controller.ts
+++ src/bookmarks/bookmarks.controller.ts
@@ -1,11 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { BookmarksService } from './bookmarks.service';
 
 @Controller('bookmarks')
 export class BookmarksController {
   constructor(private bookmarkService: BookmarksService){}
 
+  @Get()
   test(){
-    return this.bookmarkService
+    return this.bookmarkService.findAll()
   }
 }
--- src/bookmarks/bookmarks.service.ts
+++ src/bookmarks/bookmarks.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BookmarksService {}
+export class BookmarksService {
+  private bokkMarks = [{
+    id : 7,
+    name: "milad",
+    age: 22
+  }]
+
+  findAll(){
+    return this.bokkMarks
+  }
+}
