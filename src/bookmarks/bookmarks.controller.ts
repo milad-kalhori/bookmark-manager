d83c9d2a559fd9117836704c744435098372a85f
milad-kalhori <milad7kalhori@gmail.com>
2022-12-08 16:04:27 +0330

    updated bookMarks

--- src/bookmarks/bookmarks.controller.ts
+++ src/bookmarks/bookmarks.controller.ts
@@ -1,17 +1,51 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { url } from 'inspector';
+import { Bookmark } from './bookmark.model';
 import { BookmarksService } from './bookmarks.service';
+import { CreateBookmarkDto } from './dto/create-bookmark.dto';
+import { GetBookmarkDto } from './dto/get-bookmark.dto';
 
 @Controller('bookmarks')
 export class BookmarksController {
-  constructor(private bookmarkService: BookmarksService){}
+  constructor(private bookmarksService: BookmarksService) {}
 
   @Get()
-  findAll(){
-    return this.bookmarkService.findAll()
+  find(@Query() getBookmarkDto: GetBookmarkDto): Bookmark[] {
+    if (Object.keys(getBookmarkDto).length) {
+      return this.bookmarksService.find(getBookmarkDto);
+    }
+    return this.bookmarksService.findAll();
   }
 
   @Get('/:id')
-  findById(@Param('id') id: string){
-    return this.bookmarkService.findOneById()
+  findById(@Param('id') id: string): Bookmark {
+    return this.bookmarksService.findById(id);
+  }
+
+  @Post()
+  createBookmark(@Body() createBookmarkDto: CreateBookmarkDto): Bookmark {
+    return this.bookmarksService.createBookmark(createBookmarkDto);
+  }
+
+  @Delete('/:id')
+  deleteBookmark(@Param('id') id: string): void {
+    return this.bookmarksService.deleteBookmark(id);
+  }
+
+  @Patch('/:id/description')
+  updateBookmarkDescription(
+    @Param('id') id: string,
+    @Body('description') description: string,
+  ): Bookmark {
+    return this.bookmarksService.updateBookmarkDescription(id, description);
   }
 }
--- src/bookmarks/bookmarks.service.ts
+++ src/bookmarks/bookmarks.service.ts
@@ -1,20 +1,60 @@
 import { Injectable } from '@nestjs/common';
-import { threadId } from 'worker_threads';
+import { Bookmark } from './bookmark.model';
+import { v4 as uuid } from 'uuid';
+import { CreateBookmarkDto } from './dto/create-bookmark.dto';
+import { GetBookmarkDto } from './dto/get-bookmark.dto';
 
 @Injectable()
 export class BookmarksService {
-  private bookkMarks = [{
-    id : 7,
-    name: "milad",
-    age: 24
-  }]
+  private bookmarks: Bookmark[] = [];
 
-  findAll(){
-    return this.bookkMarks
+  findAll(): Bookmark[] {
+    return this.bookmarks;
   }
 
-  findOneById(id){
-    return this.bookkMarks.find((item)=> item.id === id )
+  find(getBookmarkDto: GetBookmarkDto): Bookmark[] {
+    let bookmarks = this.findAll();
+    const { url, description } = getBookmarkDto;
+
+    if (url) {
+      bookmarks = bookmarks.filter((bookmark) =>
+        bookmark.url.toLowerCase().includes(url),
+      );
+    }
+
+    if (description) {
+      bookmarks = bookmarks.filter((bookmark) =>
+        bookmark.description.toLowerCase().includes(description),
+      );
+    }
+
+    return bookmarks;
   }
 
+  findById(id: string): Bookmark {
+    return this.bookmarks.find((bookmark) => bookmark.id == id);
+  }
+
+  createBookmark(createBookmarkDto: CreateBookmarkDto): Bookmark {
+    const { url, description } = createBookmarkDto;
+    const bookmark: Bookmark = {
+      id: uuid(),
+      url,
+      description,
+    };
+
+    this.bookmarks.push(bookmark);
+
+    return bookmark;
+  }
+
+  deleteBookmark(id: string): void {
+    this.bookmarks = this.bookmarks.filter((bookmark) => bookmark.id !== id);
+  }
+
+  updateBookmarkDescription(id: string, description: string): Bookmark {
+    const bookmark = this.findById(id);
+    bookmark.description = description;
+    return bookmark;
+  }
 }
