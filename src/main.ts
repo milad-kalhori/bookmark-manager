a62dcc03e90f916780a25c7d066eb6c5bad357d1
milad-kalhori <milad7kalhori@gmail.com>
2022-12-27 16:28:48 +0330

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create();
   app.setGlobalPrefix('/v1/api');
   await app.listen(3000);
 }
