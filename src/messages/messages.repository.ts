d0854ab5f4ede1bc8a39611a0900c641c3f4b45a
milad-kalhori <milad7kalhori@gmail.com>
2023-01-04 22:02:53 +0330

    updated repository

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class MessagesRepository {}
+export class MessagesRepository {
+  findAll(){
+  }
+
+  findOne(id: string){
+    return true
+  }
+
+  create(content: string){
+  }
+
+  update(){
+
+  }
+
+  delete() {
+
+  }
+}
