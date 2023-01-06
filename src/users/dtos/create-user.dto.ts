64aa9d1d2eefc7ddf7ea4ae4c9a6af2c53d31996
milad-kalhori <milad7kalhori@gmail.com>
2023-01-06 18:12:45 +0330

    updated user dto

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -7,30 +7,3 @@ export class CreateUserDto {
   @IsString()
   password: string;
 }
-
-
-
-
-
-/*
-npm i @nestjs/cli
-//
-//packages
-// 404
-// rest client
-
-nest n car-value
-
-nest g --help
-(--flat)
-
-nest g module user
-nest g controller user
-nest g service user
-
-npm i @nestjs/typeorm typeorm sqlite3
-
-npm run start:dev
-
-npm i class-validator class-transformer
-*/
